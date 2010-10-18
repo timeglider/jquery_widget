77ddbd8b7922322a834236388e704b6766c7f2df
Pete Karl II <pkarl@Pete-Karl-IIs-MacBook-Pro-2.local>
2010-10-18 16:05:36 -0400

    added a bunch of ben stuff

---
+++ js/timeglider/timeglider.js
@@ -0,0 +1,22 @@
+/*
+* Timeglider jQuery plugin Timeglider
+* widget.timeglider.js
+* http://timeglider.com/jquery
+*
+* © 2010 Timeglider / Mnemograph LLC
+* Author: Michael Richardson
+* Licences are still to be determined : )
+*
+*/
+
+var TimeGliderWidget = {
+    _create: function() {
+        // initiatlize widget on creation
+        
+        // get jquery.timeglider plugin & GO!
+    }
+}
+
+$.widget('tg.timegliding', TimeGliderWidget);
+
+$('#somediv').timegliding();
