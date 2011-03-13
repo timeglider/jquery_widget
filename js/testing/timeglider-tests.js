9b29df8c0973e371cb0a0086c0c3cf5d8431f80c
timeglider <michael@timeglider.com>
2011-03-13 13:42:59 -0600

    just making sure master is updated before merging with feature branch... wish me luck.

--- js/testing/timeglider-tests.js
+++ js/testing/timeglider-tests.js
@@ -1,29 +1,22 @@
 /* Unit testing for timeglider */
 
-// Let's test this function
-function isEven(val) {
-	return val % 2 === 0;
-}
-
-test('testing isEven()', function() {
-	ok(isEven(0), 'Zero is an even number');
-	ok(isEven(2), 'So is two');
-	ok(isEven(-4), 'So is negative four');
-	ok(!isEven(1), 'One is not an even number');
-	ok(!isEven(-7), 'Neither is negative seven');
-});
-
 
 test("tgDate isLeapYear", function() {
-	ok(timeglider.TGDate.isLeapYear(1968), 'The year 1968 WAS a leap year!'); // there was a Feb 29 leap day!
-	ok(timeglider.TGDate.isLeapYear(2000), 'The year 2000 WAS leap year!');
-	ok(!timeglider.TGDate.isLeapYear(1900), 'The year 1900 WAS NOT a leap year!');
+	ok(timeglider.TG_Date.isLeapYear(1968), 'The year 1968 WAS a leap year!'); // there was a Feb 29 leap day!
+	ok(timeglider.TG_Date.isLeapYear(2000), 'The year 2000 WAS leap year!');
+	ok(!timeglider.TG_Date.isLeapYear(1900), 'The year 1900 WAS NOT a leap year!');
 });
 
 
-test("tgDate isValidDate", function() {
-	ok(timeglider.TGDate.isValidDate(2001, 9, 11), '2001-9-11 is a valid date');
-  ok(!timeglider.TGDate.isValidDate(2001, 21, 11), '2001-21-11 is NOT a valid date');
-  ok(!timeglider.TGDate.isValidDate(0, 7, 11), 'Year zero NOT a valid date');
-  ok(timeglider.TGDate.isValidDate(1995, 07, 11), '1995-07-11 IS a valid date');
+test("tgDate new TG_Date", function() {
+	ok(new timeglider.TG_Date("2001-09-11 12:00:00"), '2001-9-11 is a valid date');
+
+});
+
+
+
+
+test("tgDate getRataDie", function() {
+	ok(timeglider.TG_Date.getRataDie({ye:1, mo:1, da:1}), 'RataDie of 1-1-1- ought to be 1');
+
 });
