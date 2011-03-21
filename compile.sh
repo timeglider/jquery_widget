f2dc4c6291be124d70480408bb2bdfb0a5652da6
timeglider <michael@timeglider.com>
2011-03-21 15:38:09 -0600

    new min file including image loading revision

--- compile.sh
+++ compile.sh
@@ -8,4 +8,4 @@
 # jquery
 # jquery UI
 
-java -jar compiler.jar --js js/underscore-min.js --js js/backbone-min.js --js js/ba-debug.min.js --js js/jquery.tmpl.js --js js/jquery.mousewheel.min.js --js js/jquery.ui.ipad.js --js js/raphael-min.js --js js/jquery.global.js --js js/ba-tinyPubSub.js --js js/timeglider/TG_Date.js --js js/timeglider/TG_Org.js --js js/timeglider/TG_timeline.js  --js js/timeglider/TG_TimelineView.js --js js/timeglider/TG_Mediator.js --js js/timeglider/timeglider.timeline.widget.js --js_output_file js/timeglider-0.0.5.min.js
+java -jar compiler.jar --js js/underscore-min.js --js js/backbone-min.js --js js/ba-debug.min.js --js js/jquery.tmpl.js --js js/jquery.mousewheel.min.js --js js/jquery.ui.ipad.js --js js/raphael-min.js --js js/jquery.global.js --js js/ba-tinyPubSub.js --js js/timeglider/TG_Date.js --js js/timeglider/TG_Org.js --js js/timeglider/TG_timeline.js  --js js/timeglider/TG_TimelineView.js --js js/timeglider/TG_Mediator.js --js js/timeglider/timeglider.timeline.widget.js --js_output_file js/timeglider-0.0.6.min.js
