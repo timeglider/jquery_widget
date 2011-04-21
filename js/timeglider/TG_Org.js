6e6e5715f6c77a7bc2fe09fa495d633e0d60609e
timeglider <michael@timeglider.com>
2011-04-21 11:55:03 -0600

    span bars not placing properly when with image; now fixed.

--- js/timeglider/TG_Org.js
+++ js/timeglider/TG_Org.js
@@ -155,7 +155,7 @@
             b.fontsize < 10 ? b.opacity = b.fontsize / 10 : b.opacity=1;
             if (b.span == true) {
               span_selector_class = "timeglider-event-spanning";
-              span_div = "<div class='timeglider-event-spanner' style='height:" + b.fontsize + "px;width:" + b.spanwidth + "px'></div>"
+              span_div = "<div class='timeglider-event-spanner' style='top:" + title_adj + "px;height:" + b.fontsize + "px;width:" + b.spanwidth + "px'></div>"
             } else {
               span_selector_class = ""; 
               span_div = "";
