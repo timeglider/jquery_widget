13920af40b2f580834f0ce0449f1140a7d9f1e9a
timeglider <michael@timeglider.com>
2011-10-26 20:31:38 -0600

    updated ben's pubsub

--- js/ba-tinyPubSub.js
+++ js/ba-tinyPubSub.js
@@ -1,27 +1,48 @@
-/**
-* @license
-* jQuery Tiny Pub/Sub - v0.3pre - 11/4/2010
-* http://benalman.com/
-*
-* Copyright (c) 2010 "Cowboy" Ben Alman
-* Dual licensed under the MIT and GPL licenses.
-* http://benalman.com/about/license/
-*/
+/*!
+ * jQuery Tiny Pub/Sub - v0.6 - 1/10/2011
+ * http://benalman.com/
+ *
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
 
 (function($){
-  
-  var o = $({});
-  
-  $.subscribe = function(){
-    o.bind.apply( o, arguments );
+
+  // Create a "dummy" jQuery object on which to bind, unbind and trigger event
+  // handlers. Note that $({}) works in jQuery 1.4.3+, but because .unbind on
+  // a "plain object" throws errors in older versions of jQuery, an element is
+  // used here.
+  var o = $('<b/>');
+
+  // Subscribe to a topic. Works just like bind, except the passed handler
+  // is wrapped in a function so that the event object can be stripped out.
+  // Even though the event object might be useful, it is unnecessary and
+  // will only complicate things in the future should the user decide to move
+  // to a non-$.event-based pub/sub implementation.
+  $.subscribe = function( topic, fn ) {
+
+    // Call fn, stripping out the 1st argument (the event object).
+    function wrapper() {
+      return fn.apply( this, Array.prototype.slice.call( arguments, 1 ) );
+    }
+
+    // Add .guid property to function to allow it to be easily unbound. Note
+    // that $.guid is new in jQuery 1.4+, and $.event.guid was used before.
+    wrapper.guid = fn.guid = fn.guid || ( $.guid ? $.guid++ : $.event.guid++ );
+
+    // Bind the handler.
+    o.bind( topic, wrapper );
   };
-  
-  $.unsubscribe = function(){
+
+  // Unsubscribe from a topic. Works exactly like unbind.
+  $.unsubscribe = function() {
     o.unbind.apply( o, arguments );
   };
-  
-  $.publish = function(){
+
+  // Publish a topic. Works exactly like trigger.
+  $.publish = function() {
     o.trigger.apply( o, arguments );
   };
-  
+
 })(jQuery);
